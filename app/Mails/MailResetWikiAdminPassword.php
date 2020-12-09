5fb4345316994506458c2c51c385cb6ec040138e
Florestan Bredow <florestan@bredow.fr>
2020-12-09 12:26:36 +0100

    Add temporary tool to reset the WikiAdmin password of all wikis

---
+++ app/Mails/MailResetWikiAdminPassword.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Ferme\Mails;
+
+class MailResetWikiAdminPassword extends Mail
+{
+    private string $wikiAdminPassword;
+
+    public function __construct($config, $wiki, $wikiAdminPassword)
+    {
+        $this->wiki = $wiki;
+        $this->config = $config;
+        $this->wikiAdminPassword = $wikiAdminPassword;
+    }
+
+    protected function getData()
+    {
+        $data = array(
+            'name' => $this->wiki->name,
+            'url' => $this->wiki->getInfos()['url'],
+            'to' => $this->wiki->getInfos()['mail'],
+            'from' => $this->config['mail_from'],
+            'subject' => 'Installation du wiki ' . $this->wiki->name,
+            'wikiAdminPassword' => $this->wikiAdminPassword,
+            'listContacts' => $this->config['contacts']
+        );
+
+        return $data;
+    }
+
+    protected function getTemplate()
+    {
+        return "resetWikiAdminPassword.twig";
+    }
+}
