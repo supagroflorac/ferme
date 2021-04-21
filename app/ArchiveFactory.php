465789766f857a98818b9308288de73a4101e502
Florestan Bredow <florestan@bredow.fr>
2021-04-21 13:54:35 +0200

    Remove TGZ archive code

--- app/ArchiveFactory.php
+++ app/ArchiveFactory.php
@@ -3,7 +3,6 @@
 namespace Ferme;
 
 use Exception;
-use Ferme\ArchiveTgz;
 use Ferme\Configuration;
 use Ferme\ArchiveZip;
 use Ferme\Wiki\Wiki;
@@ -22,17 +21,9 @@ class ArchiveFactory
         return $this->createFromExisting(basename($archivePath));
     }
 
-    public function createFromExisting(string $filename): Archive
+    public function createFromExisting(string $filename): ArchiveZip
     {
-        // TODO Viole la regle du ouvert/Fermé. A revoir.
-        $fileExtension = substr($filename, -3);
-        if ($fileExtension === "tgz") {
-            $archive = new ArchiveTgz($filename, $this->config);
-        }
-
-        if ($fileExtension === "zip") {
-            $archive = new ArchiveZip($filename, $this->config);
-        }
+        $archive = new ArchiveZip($filename, $this->config);
 
         if (!isset($archive)) {
             throw new Exception("Type d'archive none reconnu : $filename", 1);
