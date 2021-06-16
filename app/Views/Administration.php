500fbbb82c394c202c96986d7a65c68a94744b42
Florestan Bredow <florestan@bredow.fr>
2021-06-16 09:39:39 +0200

    Delete forgotten code

--- app/Views/Administration.php
+++ app/Views/Administration.php
@@ -11,12 +11,6 @@ class Administration extends TwigView
         // Ajoute la date de dernière modification et l'espace occupés par le
         // repertoire 'files' aux informations sur le wiki.
         $listWiki = $this->ferme->wikis->search();
-        foreach ($listWiki as $wiki) {
-            $wiki->infos['LasPageModificationDateTime'] = $wiki->getLasPageModificationDateTime();
-            $wiki->infos['FilesDiskUsage'] = $wiki->getDiskUsage();
-            $wiki->infos['Release'] = $wiki->getRelease();
-            $wiki->infos['Version'] = $wiki->getVersion();
-        }
         $infos['list_wikis'] = $this->object2Infos($listWiki);
 
         $infos['list_archives'] =
